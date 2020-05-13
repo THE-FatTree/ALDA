d7e6eda8a0817eb6682094d4d536cd4c6b182f79
THE-FatTree <mark.vader.boehme@gmail.com>
2020-05-13 14:49:31 +0200

    SortedArrayDictionary compelteted TUI angelegt mehr noch nicht

---
+++ Aufgabe1/aufgabe1/TUI.java
@@ -0,0 +1,17 @@
+package aufgabe1;
+
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+
+public class TUI {
+
+    private static Dictionary<String, String>dic;
+
+    public static void main (String[] args) throws Exception {
+
+        System.out.println("Welcome to the ricefields motherfucker");
+
+        Scanner scanner = new Scanner(System.in);
+    }
+}
