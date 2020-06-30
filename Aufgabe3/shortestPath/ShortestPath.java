f4eaac54dc3b90149ccd18f0f55ea1532bb8d9fe
turboka11e <lukas.kaluscha@gmail.com>
2020-06-30 18:07:28 +0200

    searchshortestpart implementation

--- Aufgabe3/shortestPath/ShortestPath.java
+++ Aufgabe3/shortestPath/ShortestPath.java
@@ -7,105 +7,109 @@ package shortestPath;
 import shortestPath.Aufgabe2.*;
 import sim.SYSimulation;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 // ...
 
 /**
  * Kürzeste Wege in Graphen mit A*- und Dijkstra-Verfahren.
+ *
+ * @param <V> Knotentyp.
  * @author Oliver Bittel
  * @since 27.01.2015
- * @param <V> Knotentyp.
  */
 public class ShortestPath<V> {
-	
-	SYSimulation sim = null;
-	
-	Map<V,Double> dist; // Distanz für jeden Knoten
-	Map<V,V> pred; // Vorgänger für jeden Knoten
 
-	DirectedGraph<V> graph;
+    SYSimulation sim = null;
 
-	/**
-	 * Konstruiert ein Objekt, das im Graph g k&uuml;rzeste Wege 
-	 * nach dem A*-Verfahren berechnen kann.
-	 * Die Heuristik h schätzt die Kosten zwischen zwei Knoten ab.
-	 * Wird h = null gewählt, dann ist das Verfahren identisch 
-	 * mit dem Dijkstra-Verfahren.
-	 * @param g Gerichteter Graph
-	 * @param h Heuristik. Falls h == null, werden kürzeste Wege nach
-	 * dem Dijkstra-Verfahren gesucht.
-	 */
-	public ShortestPath(DirectedGraph<V> g, Heuristic<V> h) {
-		graph = g;
-	}
+    Map<V, Double> dist; // Distanz für jeden Knoten
+    Map<V, V> pred; // Vorgänger für jeden Knoten
 
-	/**
-	 * Diese Methode sollte nur verwendet werden, 
-	 * wenn kürzeste Wege in Scotland-Yard-Plan gesucht werden.
-	 * Es ist dann ein Objekt für die Scotland-Yard-Simulation zu übergeben.
-	 * <p>
-	 * Ein typische Aufruf für ein SYSimulation-Objekt sim sieht wie folgt aus:
-	 * <p><blockquote><pre>
-	 *    if (sim != null)
-	 *       sim.visitStation((Integer) v, Color.blue);
-	 * </pre></blockquote>
-	 * @param sim SYSimulation-Objekt.
-	 */
-	public void setSimulator(SYSimulation sim) {
-		this.sim = sim;
-	}
+    DirectedGraph<V> graph;
 
-	/**
-	 * Sucht den kürzesten Weg von Starknoten s zum Zielknoten g.
-	 * <p>
-	 * Falls die Simulation mit setSimulator(sim) aktiviert wurde, wird der Knoten,
-	 * der als nächstes aus der Kandidatenliste besucht wird, animiert.
-	 * @param s Startknoten
-	 * @param g Zielknoten
-	 */
-	public void searchShortestPath(V s, V g) {
-		// ...
-		List<V> kl = new LinkedList<>();
-		for(var v : graph.getVertexSet()){
-			dist.put(v, null);
-			pred.put(v, null);
-		}
-		kl.add(s);
+    /**
+     * Konstruiert ein Objekt, das im Graph g k&uuml;rzeste Wege
+     * nach dem A*-Verfahren berechnen kann.
+     * Die Heuristik h schätzt die Kosten zwischen zwei Knoten ab.
+     * Wird h = null gewählt, dann ist das Verfahren identisch
+     * mit dem Dijkstra-Verfahren.
+     *
+     * @param g Gerichteter Graph
+     * @param h Heuristik. Falls h == null, werden kürzeste Wege nach
+     *          dem Dijkstra-Verfahren gesucht.
+     */
+    public ShortestPath(DirectedGraph<V> g, Heuristic<V> h) {
+        graph = g;
+    }
 
+    /**
+     * Diese Methode sollte nur verwendet werden,
+     * wenn kürzeste Wege in Scotland-Yard-Plan gesucht werden.
+     * Es ist dann ein Objekt für die Scotland-Yard-Simulation zu übergeben.
+     * <p>
+     * Ein typische Aufruf für ein SYSimulation-Objekt sim sieht wie folgt aus:
+     * <p><blockquote><pre>
+     *    if (sim != null)
+     *       sim.visitStation((Integer) v, Color.blue);
+     * </pre></blockquote>
+     *
+     * @param sim SYSimulation-Objekt.
+     */
+    public void setSimulator(SYSimulation sim) {
+        this.sim = sim;
+    }
 
-		// UNDER CONSTRUCTION *************************************
-		while(!kl.isEmpty()) {
-			for(var adj : kl){
-				if(dist.get(adj) == null){
-					kl.add(adj);
-				}
-			}
-		}
+    /**
+     * Sucht den kürzesten Weg von Starknoten s zum Zielknoten g.
+     * <p>
+     * Falls die Simulation mit setSimulator(sim) aktiviert wurde, wird der Knoten,
+     * der als nächstes aus der Kandidatenliste besucht wird, animiert.
+     *
+     * @param s Startknoten
+     * @param g Zielknoten
+     */
+    public void searchShortestPath(V s, V g) {
+        // ...
+        Set<V> kl = new TreeSet<>();
+        for (var v : graph.getVertexSet()) {
+            dist.put(v, null);
+            pred.put(v, null);
+        }
+        kl.add(s);
+        // UNDER CONSTRUCTION *************************************
+        while (!kl.isEmpty()) {
+            for (var adj : kl) {
+                if(adj instanceof DirectedGraph) {
+                    if (dist.get(adj) == null) {
+                        kl.add(adj);
+                    }
+                }
+            }
+        }
 
-	}
+    }
 
-	/**
-	 * Liefert einen kürzesten Weg von Startknoten s nach Zielknoten g.
-	 * Setzt eine erfolgreiche Suche von searchShortestPath(s,g) voraus.
-	 * @throws IllegalArgumentException falls kein kürzester Weg berechnet wurde.
-	 * @return kürzester Weg als Liste von Knoten.
-	 */
-	public List<V> getShortestPath() {
-		// ...
-		return null;
-	}
+    /**
+     * Liefert einen kürzesten Weg von Startknoten s nach Zielknoten g.
+     * Setzt eine erfolgreiche Suche von searchShortestPath(s,g) voraus.
+     *
+     * @return kürzester Weg als Liste von Knoten.
+     * @throws IllegalArgumentException falls kein kürzester Weg berechnet wurde.
+     */
+    public List<V> getShortestPath() {
+        // ...
+        return null;
+    }
 
-	/**
-	 * Liefert die Länge eines kürzesten Weges von Startknoten s nach Zielknoten g zurück.
-	 * Setzt eine erfolgreiche Suche von searchShortestPath(s,g) voraus.
-	 * @throws IllegalArgumentException falls kein kürzester Weg berechnet wurde.
-	 * @return Länge eines kürzesten Weges.
-	 */
-	public double getDistance() {
-		// ...
-		return 0.0;
-	}
+    /**
+     * Liefert die Länge eines kürzesten Weges von Startknoten s nach Zielknoten g zurück.
+     * Setzt eine erfolgreiche Suche von searchShortestPath(s,g) voraus.
+     *
+     * @return Länge eines kürzesten Weges.
+     * @throws IllegalArgumentException falls kein kürzester Weg berechnet wurde.
+     */
+    public double getDistance() {
+        // ...
+        return 0.0;
+    }
 
 }
